5c5717e487bdb74830dbaaeab11ccd534ec34b37
sarah sajid <123872994+sarah-sajid2002@users.noreply.github.com>
2023-07-16 12:18:55 +0500

    Add files via upload

---
+++ 16-7-23/app.js
@@ -0,0 +1,60 @@
+// let Button = document.querySelector("button")
+// Button.addEventListener("click",()=>{
+//  alert("clicked")  
+// }
+// )
+
+// let user_name = document.querySelector("#user_name");
+// let button = document.querySelector("button");
+
+// button.addEventListener("click",function(){
+//     console.log(user_name.value);
+// })
+
+// function print(){
+//     console.log(user_name.value);
+// }
+
+
+// let city  = document.querySelector("#city")
+// let zip  = document.querySelector("#zip")
+// let a;
+// let button = document.querySelector("button")
+// button.addEventListener("click",function(){
+//     if(city.value === "karachi"){
+//         a = "400";
+//     }
+//     zip.value =a
+// })
+
+// let a = prompt("enter a");
+// while (a!=90){
+//     console.log("wrong");
+//     a = prompt("enter a")
+// }
+// console.log("done");
+
+// do{
+//     console.log("wrong");
+// }while(a!=90)
+
+// let str = "i am sarah"
+
+// console.log(str. replace(/[aeiou]/gi, '') );
+
+let str = "sarah";
+let count = 0;
+for(let i = 0 ;i<str.length ;i++){
+    if(str[i] == "a"){
+        count++
+    }
+
+
+}
+console.log("the occurance of a is :" + count);
+
+
+
+
+
+
